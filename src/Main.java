1a15cfeb807eebc794ed13f31abb98200637ef3f
Bollmann <oskar.bollmann@icloud.com>
2023-04-24 09:28:08 +0200

    aSLDSDFLIUSLIU

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+    }
+    private boolean besuchen(int kn)
+    {
+       //pain
+        return false;
     }
 }
