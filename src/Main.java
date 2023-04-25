74b6b7084f956504ad0ee98a606e279385e8f281
Bollmann <oskar.bollmann@icloud.com>
2023-04-26 01:21:51 +0200

    am 26.4.23 Oskar 1:00 IDE auf chromebook eingerichtet

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@ public class Main extends JFrame {
 	int xWidth = 10;
 	int yWidth = 10;
 	public int[][] arrayTest = new int[yWidth][xWidth];
-
-	/* ={
-		{0, 1, 0},
-		{0, 0, 0},
-		{0, 0, 0},
-		{0, 0, 0},
-		{0, 0, 0}
-	};*/
 	public UI ui;
 	public int col = arrayTest[0].length;
 	public int row = arrayTest.length;
@@ -27,7 +19,7 @@ public class Main extends JFrame {
 
 	private void main2() {
 		populateArr();
-		ui = new UI(arrayTest[0].length*100, arrayTest.length*100);
+		ui = new UI(arrayTest[0].length*100, arrayTest.length*100, arrayTest);
 		circler();
 	}
 
@@ -52,17 +44,19 @@ public class Main extends JFrame {
 		refresh.start();*/
 
 		Thread add2sim = new Thread(() -> {
-			while (true) {
+			boolean tr = true;
+			while (tr) {
 				arrayTest[0][5/*rng.nextInt(xWidth)*/] = rng.nextInt(11);
 				try {
-					TimeUnit.SECONDS.sleep(5);
+					TimeUnit.MILLISECONDS.sleep(100);
 				} catch (InterruptedException ignored) {
 				}
+				tr=false;
 			}
 		});
 		add2sim.start();
 	}
-//NOTE:integrate visual update into updatePos()
+//TODO:integrate visual update into updatePos()
 	private void updatePos() {
 
 		for (int y = 0; y < row; y++) {
@@ -112,26 +106,10 @@ public class Main extends JFrame {
 						}
 					}
 				}
-/*				if ((y + 1 < row) && (x - 1 < col) && rng.nextBoolean() == true) {
-					System.out.println("got here ");
-					if (arrayTest[y + 1][x - 1] == 0) {
-
-						arrayTest[y + 1][x - 1] = arrayTest[y][x];
-					} else {
-						arrayTest[y + 1][x + 1] = arrayTest[y][x];
-					}
-					arrayTest[y][x] = 0;
-				} else if (y + 1 < row && x + 1 < col) {
-					System.out.println("got here 2");
-					if (arrayTest[y + 1][x + 1] == 0) {
-						arrayTest[y + 1][x + 1] = arrayTest[y][x];
-					} else {
-						arrayTest[y + 1][x - 1] = arrayTest[y][x];
-					}
-					arrayTest[y][x] = 0;
-				}*/
+				//TODO: OBSERVER PATTERN: only update when
+				ui.updateAtPos(y, x);
 			}
-			Thread refresh = new Thread(() -> {
+			/*Thread refresh = new Thread(() -> {
 				ui.updateGrid(arrayTest);
 			});
 			refresh.start();
@@ -139,7 +117,7 @@ public class Main extends JFrame {
 			try {
 				TimeUnit.MILLISECONDS.sleep(500);
 			} catch (InterruptedException ignored) {
-			}
+			}*/
 		}
 	}
 
@@ -147,5 +125,8 @@ public class Main extends JFrame {
 		for (int y = 0; y < row; y++) {
 			Arrays.fill(arrayTest[y], 0);
 		}
+	}
+	public int[][] getArrayTest(){
+		return arrayTest;
 	}
 }
--- src/UI.java
+++ src/UI.java
@@ -4,10 +4,11 @@ import java.awt.*;
 
 public class UI extends JFrame {
 	public Panel p;
-int[][] arr = Main.arrayTest;
-GridBagConstraints gbc;
+	int[][] arr;
+	GridBagConstraints gbc;
 
-	UI(int width, int height) {
+	UI(int width, int height, int[][] arrayTest) {
+		arr = arrayTest;
 		p = new Panel();
 		p.setVisible(true);
 		p.setPreferredSize(new Dimension(width, height));
@@ -20,13 +21,11 @@ GridBagConstraints gbc;
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 
-			 gbc = new GridBagConstraints();
-    gbc.gridx = colIndex;
-    gbc.gridy = rowIndex;
-    gbc.fill = GridBagConstraints.BOTH;
-    gbc.weightx = 1.0;
-    gbc.weighty = 1.0;
-    gbc.insets = new Insets(0, 0, 0, 0);
+		gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.insets = new Insets(0, 0, 0, 0);
 	}
 
 	public void updateGrid() {
@@ -38,7 +37,7 @@ GridBagConstraints gbc;
 		for (int y = 0; y < row; y++) {
 			for (int x = 0; x < col; x++) {
 				JPanel cell = new JPanel();
-				color=colorAtPos(x,y);
+				Color color = colorAtPos(x, y);
 				cell.setBackground(color);
 				if (color != Color.white)
 					cell.setBorder(BorderFactory.createLineBorder(Color.red));
@@ -50,34 +49,38 @@ GridBagConstraints gbc;
 		p.revalidate();
 		p.repaint();
 	}
-public void updateAtPos(int row, int col) {
-    Component comp = p.getComponentAt(col, row);
-    p.remove(comp);
 
-				JPanel cell = new JPanel();
+	public void updateAtPos(int row, int col) {
+		/*Component comp = p.getComponentAt(col, row);
+		p.remove(comp);*/
 
-    cell.setBackground(colorAtPos(col,row); //NOTE: inout color not only yellow
-    
-    p.add(cell, gbc);
-    
-    p.revalidate();
-    p.repaint();
-}
-private Color colorAtPos(int x, int y){
-return switch (arr[y][x]) { // color enhanced switch
-					case 1 -> Color.red;
-					case 2 -> Color.black;
-					case 3 -> Color.GREEN;
-					case 4 -> Color.gray;
-					case 5 -> Color.BLUE;
-					case 6 -> Color.CYAN;
-					case 7 -> Color.MAGENTA;
-					case 8 -> Color.PINK;
-					case 9 -> Color.yellow;
-					case 10 -> Color.orange;
-					default -> Color.white;
-				};
-}
+		JPanel cell = new JPanel();
+
+		cell.setBackground(colorAtPos(col, row));
+
+		gbc.gridx = col;
+		gbc.gridy = row;
+		p.add(cell, gbc);
+
+		p.revalidate();
+		//p.repaint();
+	}
+
+	private Color colorAtPos(int x, int y) {
+		return switch (arr[y][x]) { // color enhanced switch
+			case 1 -> Color.red;
+			case 2 -> Color.black;
+			case 3 -> Color.GREEN;
+			case 4 -> Color.gray;
+			case 5 -> Color.BLUE;
+			case 6 -> Color.CYAN;
+			case 7 -> Color.MAGENTA;
+			case 8 -> Color.PINK;
+			case 9 -> Color.yellow;
+			case 10 -> Color.orange;
+			default -> Color.white;
+		};
+	}
 
 	/*public void cube(int _x, int _y, int _w, int _h) {
 		Cube x =new Cube(_x, _y, _w, _h);
